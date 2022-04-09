6e127df2f59219398132e6e0333badc0083e27d1
Bhaskor Jyoti Sarmah <bhaskor.sarmah@privacera.com>
2022-04-09 14:39:02 +0530

    Implementation Done for handing two numbers

--- src/main/java/main/TDDMain.java
+++ src/main/java/main/TDDMain.java
@@ -8,6 +8,9 @@ public class TDDMain {
   public int add(String number) {
     if (number.isEmpty()) {
       return 0;
+    } else if (number.contains(",")) {
+      String numbers[] = number.split(",");
+      return Integer.parseInt(numbers[0].trim()) + Integer.parseInt(numbers[1].trim());
     } else {
       return Integer.parseInt(number);
     }
